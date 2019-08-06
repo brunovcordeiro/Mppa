773a93901712df7e0c943f4ef180fa3825c2fb06
brunovcordeiro <bruno_vcordeiro@hotmail.com>
2019-08-06 00:20:51 -0300

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,39 +1,25 @@
-#include <iostream>
-#include <intrin.h>
-
-using namespace std;
-
-int _tmain(int argc, _TCHAR* argv[])
-{
-    // Get extended ids.
-    int CPUInfo[4] = {-1};
-    __cpuid(CPUInfo, 0x80000000);
-    unsigned int nExIds = CPUInfo[0];
-
-    // Get the information associated with each extended ID.
-    char CPUBrandString[0x40] = { 0 };
-    for( unsigned int i=0x80000000; i<=nExIds; ++i)
-    {
-        __cpuid(CPUInfo, i);
-
-        // Interpret CPU brand string and cache information.
-        if  (i == 0x80000002)
-        {
-            memcpy( CPUBrandString,
-            CPUInfo,
-            sizeof(CPUInfo));
-        }
-        else if( i == 0x80000003 )
-        {
-            memcpy( CPUBrandString + 16,
-            CPUInfo,
-            sizeof(CPUInfo));
-        }
-        else if( i == 0x80000004 )
-        {
-            memcpy(CPUBrandString + 32, CPUInfo, sizeof(CPUInfo));
-        }
-}
-
-    cout << "Cpu String: " << CPUBrandString;
+#include <sys/utsname.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/utsname.h>
+ 
+int main(void){
+	int i;
+	struct utsname myname;
+	i = uname(&myname); /* hold the structure */
+	if ( i == 0 ){
+		printf("Operating system name : %s\n",myname.sysname);
+		printf("Node name : %s\n",myname.nodename);
+		printf("Operating system release : %s\n",myname.release);
+		printf("Operating system name : %s\n",myname.version);
+		printf("Hardware identifier : %s\n",myname.machine);
+	}
+	else {
+		 fprintf(stderr,"Oh no. uname(2) failed with %s\n", strerror(errno));
+		 exit(1);
+	}
+	return 0;
 }
