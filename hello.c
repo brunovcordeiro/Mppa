06176afa7c11058636716508d77892dcfab49e57
brunovcordeiro <bruno_vcordeiro@hotmail.com>
2019-08-06 00:25:06 -0300

    Update hello.c

--- hello.c
+++ hello.c
@@ -7,5 +7,19 @@
 #include <sys/utsname.h>
  
 int main(void){
-system("lscpu");
+	int i;
+	struct utsname myname;
+	i = uname(&myname); /* hold the structure */
+	if ( i == 0 ){
+		printf("Operating system name : %s\n",myname.sysname);
+		printf("Node name : %s\n",myname.nodename);
+		printf("Operating system release : %s\n",myname.release);
+		printf("Operating system name : %s\n",myname.version);
+		printf("Hardware identifier : %s\n",myname.machine);
+	}
+	else {
+		 fprintf(stderr,"Oh no. uname(2) failed with %s\n", strerror(errno));
+		 exit(1);
+	}
+	return 0;
 }
